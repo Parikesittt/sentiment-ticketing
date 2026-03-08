1de727a25a8b05eb7c6ee8eaa4e55439cdc1fcb9
Parikesittt <parikesit.widodo@gmail.com>
2026-03-08 17:14:10 +0700

    add cors

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ from pydantic import BaseModel
 import requests
 import os
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 load_dotenv()
 HF_TOKEN = os.getenv("HF_TOKEN")
